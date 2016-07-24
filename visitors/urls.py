1e9baf4991ff1d769f13097fae20d09ace0e8509
Dragon Lin <Dragon Lin>
2016-07-24 23:24:15 +0800

    setup django template and render it

--- visitors/urls.py
+++ visitors/urls.py
@@ -2,6 +2,10 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'visitors'
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<visitor_id>[0-9]+)/$', views.get_visitor, name='visitor'),
+    url(r'^(?P<month>[0-9]{4}-[0-9]{2})/$', views.visitor_reason, name='reason'),
+    url(r'^(?P<month>[0-9]{4}-[0-9]{2})/(?P<reason>\w+ \w+( \w+)?)/$', views.visitor_detail, name='detail')
 ]
--- visitors/views.py
+++ visitors/views.py
@@ -1,6 +1,36 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Visitor
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the visitors index.")
+    return render(request, 'visitors/index.html', {})
+
+
+def visitor_reason(request, month):
+    visitor_reasons = Visitor.objects.filter(report_month=month)
+    visitor_reasons_distinct = set()
+    for visitor in visitor_reasons:
+        visitor_reasons_distinct.add(visitor.reason)
+    context = {
+        'visitor_reasons_distinct': visitor_reasons_distinct,
+        'month': month,
+    }
+    return render(request, 'visitors/reason.html', context)
+
+
+def visitor_detail(request, month, reason):
+    visitor_record_by_reason = Visitor.objects.filter(report_month=month, reason=reason)
+    context = {
+        'visitor_record_by_reason': visitor_record_by_reason,
+        'month': month
+    }
+    return render(request, 'visitors/detail.html', context)
+
+
+def get_visitor(request, visitor_id):
+    t = get_object_or_404(Visitor, pk=visitor_id)
+    context = {
+            'visitor': t,
+        }
+    return render(request, 'visitors/visitor.html', context)
+
