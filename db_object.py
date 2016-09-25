adc3c979925031fa6f3ec1ff8d950d3e86f0c298
Dragon Lin <Dragon Lin>
2016-09-26 00:42:45 +0800

    add bind variable method and print sql function

--- db_object.py
+++ db_object.py
@@ -8,17 +8,34 @@ class SqliteDBObject:
         self.cursor = self.conn.cursor()
 
     def non_select_query(self, sql, bind_var=None, print_sql=False):
+        if print_sql:
+            self.print_sql_str(sql, bind_var)
+
         if bind_var is None:
             self.cursor.execute(sql)
         else:
             self.cursor.execute(sql, bind_var)
+
         self.conn.commit()
 
+    def select_query(self, sql, bind_var=None, print_sql=False):
         if print_sql:
-            print(sql.replace('?', '{}').format(*bind_var))
+            self.print_sql_str(sql, bind_var)
 
-    def select_query(self, sql):
-        return self.cursor.execute(sql)
+        if bind_var is None:
+            return self.cursor.execute(sql)
+        else:
+            return self.cursor.execute(sql, bind_var)
+
+    def print_sql_str(self, sql, bind_var):
+        if bind_var is None:
+            sql_str = sql
+        else:
+            bind_var = map(lambda var: "'" + var + "'" if isinstance(var, str)
+                           else var, bind_var)
+            sql_str = sql.replace('?', '{}').format(*bind_var)
+
+        print(sql_str)
 
     def db_close(self):
         self.conn.close()
