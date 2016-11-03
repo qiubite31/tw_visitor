25b150f683d0bf6a98b07a63d8725c4133cd2a94
Dragon Lin <Dragon Lin>
2016-11-03 23:32:43 +0800

    fix interger field set max_length

--- visitors/models.py
+++ visitors/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class ArrivalRecord(models.Model):
-    report_year = models.IntegerField(max_length=4, default=0000)
-    report_month = models.IntegerField(max_length=2, default=00)
+    report_year = models.IntegerField(default=0000)
+    report_month = models.IntegerField(default=00)
     continent = models.CharField(max_length=100)
     area_cht = models.CharField(max_length=200)
     area_eng = models.CharField(max_length=200)
